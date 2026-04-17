ddac82eacca05a099a93655613092fd82f43dfdf
JEWEL HOSSAIN <jewelhossain9091@gmail.com>
2026-04-17 15:59:46 +0530

    Time: 1 ms (57.78%), Space: 8.4 MB (91.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long rev = 0;
+        if(x<0) return false;
+        int n=x;
+        while(x!=0){
+            int digit = x % 10;
+            rev = rev*10 + digit;
+            x = x / 10;
+        }
+        if(rev == n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
