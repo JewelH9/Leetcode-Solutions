4ca53e5fca0f1f4151fdb7888494f01834ac649d
JEWEL HOSSAIN <jewelhossain9091@gmail.com>
2026-04-22 15:53:38 +0530

    Time: 0 ms (100%), Space: 11.8 MB (62.72%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+
+        string sub="";
+
+        for(int i=0; i < strs[0].size(); i++){
+            char ch = strs[0][i];
+
+            for(int j=1; j < strs.size(); j++){
+                if(i >= strs[j].size() || strs[j][i] != ch){
+                    return sub;
+                }
+            }
+            sub += ch;
+        }
+        return sub;
+    }
+};
